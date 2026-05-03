a3fac9ceb214d836183e275028a40a3f221a6503
Oxxed Lockok <oxeed@mail.ru>
2026-05-03 19:09:25 +0300

    feature/statistics

--- cmd/todoapp/main.go
+++ cmd/todoapp/main.go
@@ -13,6 +13,9 @@ import (
 	core_pgx_pool "github.com/Lockok/golang-niltodo/internal/core/repository/postgres/pool/pgx"
 	core_http_middleware "github.com/Lockok/golang-niltodo/internal/core/transport/http/middleware"
 	core_http_server "github.com/Lockok/golang-niltodo/internal/core/transport/http/server"
+	statistics_postgres_repository "github.com/Lockok/golang-niltodo/internal/features/statistics/repository/postgres"
+	statistics_service "github.com/Lockok/golang-niltodo/internal/features/statistics/service"
+	statistics_transport_http "github.com/Lockok/golang-niltodo/internal/features/statistics/transport/http"
 	tasks_postgres_repository "github.com/Lockok/golang-niltodo/internal/features/tasks/repository/postgres"
 	tasks_service "github.com/Lockok/golang-niltodo/internal/features/tasks/service"
 	tasks_transport_http "github.com/Lockok/golang-niltodo/internal/features/tasks/transport/http"
@@ -58,12 +61,18 @@ func main() {
 	tasksService := tasks_service.NewTasksService(tasksRepository)
 	tasksTransportHTTP := tasks_transport_http.NewTasksHTTPHandler(tasksService)
 
+	logger.Debug("initializing feature", zap.String("feature", "statistics"))
+	statisticsRepository := statistics_postgres_repository.NewStatisticsRepository(pool)
+	statisticsService := statistics_service.NewStatisticsService(statisticsRepository)
+	statisticsTransportHTTP := statistics_transport_http.NewStatisticsHTTPHandler(statisticsService)
+
 	logger.Debug("initializng HTTP server")
 	httpServer := core_http_server.NewHTTPServer(core_http_server.NewConfigMust(), logger, core_http_middleware.RequestID(), core_http_middleware.Logger(logger), core_http_middleware.Trace(), core_http_middleware.Panic())
 
 	apiVersionRouterV1 := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion1)
 	apiVersionRouterV1.RegisterRoutes(usersTransportHTTP.Routes()...)
 	apiVersionRouterV1.RegisterRoutes(tasksTransportHTTP.Routes()...)
+	apiVersionRouterV1.RegisterRoutes(statisticsTransportHTTP.Routes()...)
 
 	// apiVersionRouterV2 := core_http_server.NewAPIVersionRouter(core_http_server.ApiVersion2, core_http_middleware.Dummy("api v2 middleware"))
 	// apiVersionRouterV2.RegisterRoutes(usersTransportHTTP.Routes()...)
